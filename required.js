26bda0a89da16a0c1e9e322566d397b3e5230b7f
ts5-25 <104970913+ts5-25@users.noreply.github.com>
2023-03-23 11:45:44 +0900

    必修データの追加

--- required.js
+++ required.js
@@ -4,18 +4,103 @@ const data = JSON.parse(rawData);
 const required = {};
 const subjectName = {};
 
+
 data.forEach(e => {
     for (let i = 1; i < 40; i++) {
-        const className = "s1_" + i;
+        const className_s1 = "s1_" + i;
         for (let i = 0; i < e.one_grade.length; i++) {
-            if (className === e.one_grade[i]) {
-                if (className in required) {
-                    required[className].push(e.code);
-                    subjectName[className].push(e.titleJp);
+            if (className_s1 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
                 } else {
-                    required[className] = [e.code];
-                    subjectName[className] = [e.titleJp];
+                    if (className_s1 in required) {
+                        required[className_s1].push(e.code);
+                        subjectName[className_s1].push(e.titleJp);
+                    } else {
+                        required[className_s1] = [e.code];
+                        subjectName[className_s1] = [e.titleJp];
+                    }
                 }
+                
+            }
+        }
+        const className_s2 = "s2_" + i;
+        for (let i = 0; i < e.one_grade.length; i++) {
+            if (className_s2 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
+                } else {
+                    if (className_s2 in required) {
+                        required[className_s2].push(e.code);
+                        subjectName[className_s2].push(e.titleJp);
+                    } else {
+                        required[className_s2] = [e.code];
+                        subjectName[className_s2] = [e.titleJp];
+                    }
+                }
+                
+            }
+        }
+        const className_s3 = "s3_" + i;
+        for (let i = 0; i < e.one_grade.length; i++) {
+            if (className_s3 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
+                } else {
+                    if (className_s3 in required) {
+                        required[className_s3].push(e.code);
+                        subjectName[className_s3].push(e.titleJp);
+                    } else {
+                        required[className_s3] = [e.code];
+                        subjectName[className_s3] = [e.titleJp];
+                    }
+                }
+                
+            }
+        }
+        const className_l1 = "l1_" + i;
+        for (let i = 0; i < e.one_grade.length; i++) {
+            if (className_l1 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
+                } else {
+                    if (className_l1 in required) {
+                        required[className_l1].push(e.code);
+                        subjectName[className_l1].push(e.titleJp);
+                    } else {
+                        required[className_l1] = [e.code];
+                        subjectName[className_l1] = [e.titleJp];
+                    }
+                }
+                
+            }
+        }
+        const className_l2 = "l2_" + i;
+        for (let i = 0; i < e.one_grade.length; i++) {
+            if (className_l2 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
+                } else {
+                    if (className_l2 in required) {
+                        required[className_l2].push(e.code);
+                        subjectName[className_l2].push(e.titleJp);
+                    } else {
+                        required[className_l2] = [e.code];
+                        subjectName[className_l2] = [e.titleJp];
+                    }
+                }
+                
+            }
+        }
+        const className_l3 = "l3_" + i;
+        for (let i = 0; i < e.one_grade.length; i++) {
+            if (className_l3 === e.one_grade[i]) {
+                if (e.titleJp === '基礎化学' || e.titleJp === '基礎統計' || e.titleJp === '力学Ｂ') {
+                } else {
+                    if (className_l3 in required) {
+                        required[className_l3].push(e.code);
+                        subjectName[className_l3].push(e.titleJp);
+                    } else {
+                        required[className_l3] = [e.code];
+                        subjectName[className_l3] = [e.titleJp];
+                    }
+                }
+                
             }
         }
     }
@@ -23,3 +108,4 @@ data.forEach(e => {
 
 fs.writeFileSync("required.json", JSON.stringify(required));
 console.log(subjectName);
+//console.log(nonRequired["s1"]);
