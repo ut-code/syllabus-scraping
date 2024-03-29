f4bc9e9566c1783ee6313bd9fee96a7cdd163bea
NISHIYAMA-Takuki <nishiyama.takuki@ai-li.co.jp>
2024-03-30 05:31:31 +0900

    バージョン周り再修正, 重複ロジック切り出し

--- scrape.js
+++ scrape.js
@@ -26,6 +26,39 @@ const getDetailsUrl = (code, key, term) =>
     locale: "ja_JP",
   });
 
+const loadSearchPanelAndGetUrl = async (page) => {
+  await page.goto(
+    "https://utas.adm.u-tokyo.ac.jp/campusweb/campusportal.do?page=main&tabId=sy"
+  );
+  await wait(1000);
+  const url = await page.$eval("iframe#main-frame-if", (iframe) => iframe.src);
+  return url;
+};
+
+const loadSearchResult = async (page, url, gakkiKubunCode) => {
+  await page.goto(url);
+  await page.$eval("input#nendo", (input) => {
+    input.value = nendo;
+  });
+  await wait(2000);
+  await page.$eval("select#gakubuShozokuCode", (select) => {
+    select.value = "00";
+  });
+  await wait(2000);
+  await page.$eval("select#gakkiKubunCode", (select) => {
+    select.value = gakkiKubunCode;
+  }); // S1, S2, A1, A2 の順で 3 ~ 6
+  await wait(2000);
+  await page.$eval(
+    "select[name=_displayCount] option[selected]",
+    (option) => (option.value = "5000")
+  );
+  await wait(1000);
+  await page
+    .$x("//input[contains(@value, '検索')]")
+    .then(([searchButton]) => searchButton.click());
+};
+
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   console.log("Browser launched.");
@@ -36,96 +69,15 @@ const getDetailsUrl = (code, key, term) =>
   /* ここで手動でログイン処理を行う */
   await wait(60000);
 
-  await page.goto(
-    "https://utas.adm.u-tokyo.ac.jp/campusweb/campusportal.do?page=main&tabId=sy"
-  );
-  await wait(1000);
-  const url = await page.$eval("iframe#main-frame-if", (iframe) => iframe.src);
-  await page.goto(url);
-  await page.$eval("input#nendo", (input) => {
-    input.value = nendo;
-  });
-  await wait(2000);
-  await page.$eval("select#gakubuShozokuCode", (select) => {
-    select.value = "00";
-  });
-  await wait(2000);
-  await page.$eval("select#gakkiKubunCode", (select) => {
-    select.value = semester === "S" ? 3 : 5;
-  }); // S1, S2, A1, A2 の順で 3 ~ 6
-  await wait(2000);
-  await page.$eval(
-    "select[name=_displayCount] option[selected]",
-    (option) => (option.value = "5000")
-  );
-  await wait(1000);
-  await page
-    .$x("//input[contains(@value, '検索')]")
-    .then(([searchButton]) => searchButton.click());
-  await wait(30000);
+  const url = await loadSearchPanelAndGetUrl(page);
 
-  let basicInfos = await page.$$eval("tbody tr", (rows) =>
-    rows.map((row) => {
-      const [
-        ,
-        semester,
-        _periods,
-        ,
-        ,
-        classroom,
-        ,
-        code,
-        _type,
-        category,
-        titleJp,
-        lecturerJp,
-      ] = [...row.children].map((item) => item.innerText.trim());
-      const periods = _periods
-        .replace(/\s/, "")
-        .split(",")
-        .map((period) => period.trim());
-      const type = _type.replace(/(.+)科目/, "$1");
-
-      return {
-        code,
-        type,
-        category,
-        semester,
-        periods,
-        classroom,
-        titleJp,
-        lecturerJp,
-      };
-    })
-  );
-
-  const num_of_A1 = basicInfos.length;
-
-  await page.goto(url);
-  await page.$eval("input#nendo", (input) => {
-    input.value = nendo;
-  });
-  await wait(2000);
-  await page.$eval("select#gakubuShozokuCode", (select) => {
-    select.value = "00";
-  });
-  await wait(2000);
-  await page.$eval("select#gakkiKubunCode", (select) => {
-    select.value = semester === "S" ? 4 : 6;
-  }); // S1, S2, A1, A2 の順で 3 ~ 6
-  await wait(2000);
-  await page.$eval(
-    "select[name=_displayCount] option[selected]",
-    (option) => (option.value = "5000")
-  );
-  await wait(1000);
-  await page
-    .$x("//input[contains(@value, '検索')]")
-    .then(([searchButton]) => searchButton.click());
-  await wait(30000);
-
-  basicInfos = basicInfos.concat(
-    await page.$$eval("tbody tr", (rows) =>
+  const basicInfos = [];
+  const dataNums = [];
+  const gakkiKubunCodes = semester === "S" ? [3, 4] : [5, 6];
+  for (const gakkiKubunCode of gakkiKubunCodes) {
+    await loadSearchResult(page, url, gakkiKubunCode);
+    await wait(30000);
+    const termBasicInfos = await page.$$eval("tbody tr", (rows) =>
       rows.map((row) => {
         const [
           ,
@@ -158,8 +110,12 @@ const getDetailsUrl = (code, key, term) =>
           lecturerJp,
         };
       })
-    )
-  );
+    );
+    basicInfos = basicInfos.concat(termBasicInfos);
+    dataNums.push(termBasicInfos.length);
+  }
+
+  const numOfTerm1 = dataNums[0];
 
   console.log(basicInfos.length);
 
@@ -173,7 +129,11 @@ const getDetailsUrl = (code, key, term) =>
       })`
     );
     await page.goto(
-      getDetailsUrl(basicInfo.code, flowExecutionKey, i < num_of_A1 ? "5" : "6")
+      getDetailsUrl(
+        basicInfo.code,
+        flowExecutionKey,
+        gakkiKubunCodes[i < numOfTerm1 ? 0 : 1]
+      )
     );
     await wait(2500);
     await page.$$eval(".ui-tabs-hide", (elements) => {
@@ -235,36 +195,9 @@ const getDetailsUrl = (code, key, term) =>
       fs.writeFileSync(`data${version}.json`, JSON.stringify(data));
     }
 
-    if (i + 1 === num_of_A1) {
-      await page.goto(
-        "https://utas.adm.u-tokyo.ac.jp/campusweb/campusportal.do?page=main&tabId=sy"
-      );
-      await wait(1000);
-      const url = await page.$eval(
-        "iframe#main-frame-if",
-        (iframe) => iframe.src
-      );
-      await page.goto(url);
-      await page.$eval("input#nendo", (input) => {
-        input.value = "2023";
-      });
-      await wait(2000);
-      await page.$eval("select#gakubuShozokuCode", (select) => {
-        select.value = "00";
-      });
-      await wait(2000);
-      await page.$eval("select#gakkiKubunCode", (select) => {
-        select.value = "6";
-      }); // S1, S2, A1, A2 の順で 3 ~ 6
-      await wait(2000);
-      await page.$eval(
-        "select[name=_displayCount] option[selected]",
-        (option) => (option.value = "5000")
-      );
-      await wait(1000);
-      await page
-        .$x("//input[contains(@value, '検索')]")
-        .then(([searchButton]) => searchButton.click());
+    if (i + 1 === numOfTerm1) {
+      const url = await loadSearchPanelAndGetUrl(page);
+      await loadSearchResult(page, url, gakkiKubunCodes[1]);
       await wait(10000);
       flowExecutionKey = await page.evaluate(
         () => location.href.match(/flowExecutionKey=(.+)$/)[1]
@@ -272,40 +205,12 @@ const getDetailsUrl = (code, key, term) =>
     }
 
     if (i % 50 === 0) {
-      await page.goto(
-        "https://utas.adm.u-tokyo.ac.jp/campusweb/campusportal.do?page=main&tabId=sy"
+      const url = await loadSearchPanelAndGetUrl(page);
+      await loadSearchResult(
+        page,
+        url,
+        gakkiKubunCodes[i < numOfTerm1 ? 0 : 1]
       );
-      await wait(1000);
-      const url = await page.$eval(
-        "iframe#main-frame-if",
-        (iframe) => iframe.src
-      );
-      await page.goto(url);
-      await page.$eval("input#nendo", (input) => {
-        input.value = "2023";
-      });
-      await wait(2000);
-      await page.$eval("select#gakubuShozokuCode", (select) => {
-        select.value = "00";
-      });
-      await wait(2000);
-      await page.$eval(
-        "select#gakkiKubunCode",
-        (select, i, num_of_A1) => {
-          select.value = i < num_of_A1 ? "5" : "6";
-        },
-        i,
-        num_of_A1
-      ); // S1, S2, A1, A2 の順で 3 ~ 6
-      await wait(2000);
-      await page.$eval(
-        "select[name=_displayCount] option[selected]",
-        (option) => (option.value = "5000")
-      );
-      await wait(1000);
-      await page
-        .$x("//input[contains(@value, '検索')]")
-        .then(([searchButton]) => searchButton.click());
       await wait(10000);
       flowExecutionKey = await page.evaluate(
         () => location.href.match(/flowExecutionKey=(.+)$/)[1]
