dabb8aa957b654063c3e55fa2cfeebd14a75a79c
NISHIYAMA-Takuki <nishiyama.takuki@ai-li.co.jp>
2024-04-02 12:26:20 +0900

    sort.jsリファクタリング

--- sort.js
+++ sort.js
@@ -1,4 +1,5 @@
 // 授業種別によってDBをソート
+// TODO: process.jsの後に実行するように改修
 
 const fs = require("fs");
 
@@ -11,63 +12,57 @@ const rawData = fs.readFileSync(readFileName).toString();
 
 const data = JSON.parse(rawData);
 
-const peak = [];
-const Jp = [];
-const compulsory_subject = [];
-const general_subject = {
-  Ａ: [],
-  Ｂ: [],
-  Ｃ: [],
-  Ｄ: [],
-  Ｅ: [],
-  Ｆ: [],
-  Ｌ: [],
-};
-const gen_keys = Object.keys(general_subject);
-const requested_subject = [];
-const theme_subject = [];
-const broad_subject = [];
-// const sophomore = [];
-// const rest = []; // 残りの優先度高めの授業
+/** @type {Map<string, Lecture[]>} */
+const nonIntegrated = new Map([
+  ["基礎", []],
+  ["主題", []],
+  ["要求", []],
+  ["展開", []],
+  ["PEAK", []],
+  ["JP", []],
+]);
+/** @type {Map<string, Lecture[]>} */
+const integrated = new Map([
+  ["A", []],
+  ["B", []],
+  ["C", []],
+  ["D", []],
+  ["E", []],
+  ["F", []],
+  ["L", []],
+]);
 
 data.forEach((e) => {
   if (e.titleJp.includes("PEAK")) {
-    peak.push(e);
+    nonIntegrated.get("PEAK").push(e);
   } else if (e.titleJp.includes("日本語") && e.titleJp.includes("級")) {
-    Jp.push(e);
-  } else if (e.type === "基礎") {
-    compulsory_subject.push(e);
-  } else if (e.type === "総合") {
-    for (key of gen_keys) {
-      if (e.category.includes(key)) {
-        general_subject[key].push(e);
-        break;
-      }
-    }
-  } else if (e.type === "要求") {
-    requested_subject.push(e);
-  } else if (e.type === "主題") {
-    theme_subject.push(e);
-  } else if (e.type === "展開") {
-    broad_subject.push(e);
+    nonIntegrated.get("JP").push(e);
+  } else if (nonIntegrated.has(e.type)) {
+    nonIntegrated.get(e.type).push(e);
+  } else if (e.type === "総合" && integrated.has(e.shortenedCategory)) {
+    integrated.get("JP").push(e);
   } else {
-    console.log(e.type, e.titleJp);
+    console.log(e.type, e.shortenedCategory, e.titleJp);
   }
 });
 
-let sorted = compulsory_subject;
-console.log(compulsory_subject.length, sorted.length);
-for (key of gen_keys) {
-  sorted = sorted.concat(general_subject[key]);
-  console.log(general_subject[key].length, sorted.length);
-}
-console.log(sorted.length);
-sorted = sorted.concat(
-  theme_subject,
-  requested_subject,
-  broad_subject,
-  peak,
-  Jp
+console.log("総合");
+integrated.forEach((v, k) => {
+  console.log(`${k}: ${v.length}`);
+});
+console.log("");
+nonIntegrated.forEach((v, k) => {
+  console.log(`${k}: ${v.length}`);
+});
+
+const foundation = nonIntegrated.get("基礎");
+nonIntegrated.delete("基礎");
+
+const sorted = foundation.concat(
+  ...integrated.values(),
+  ...nonIntegrated.values()
 );
+
+console.log(sorted.length);
 
 fs.writeFileSync(writeFileName, JSON.stringify(sorted));
