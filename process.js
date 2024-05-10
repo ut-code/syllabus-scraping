6b7814c43b11ab18a8931f0ee91bd1e9e5a96d71
NISHIYAMA-Takuki <95198404+NISHIYAMA-Takuki@users.noreply.github.com>
2024-05-10 23:34:08 +0900

    refactor class parser

--- process.js
+++ process.js
@@ -76,198 +76,101 @@ const normalizeText = (text) =>
     .replace(/(【Zoom URLはポータル等にてご確認ください】)(\\n| )*\1/g, "$1"); // ZoomURL削除
 
 /**
+ * 学年をパースして数字として返す。学年でない場合はnull
+ * @param {string} text
+ * @returns {number | null}
+ */
+const parseGrade = (text) => {
+  return text[1] === "年" ? Number(text[0]) : null;
+};
+
+/**
+ * 科類をパースして科類コードの一部もしくは全部を返す。科類でない場合はnull
+ * @param {string} text
+ * @returns {[boolean, string[]] | null} 第一要素はコードの全部(true)か一部(false)か, 第二要素はコードのリスト
+ */
+const parseStream = (text) => {
+  const category = { 文: "l", 理: "s" }[text[0]];
+  if (!category) {
+    return null;
+  }
+  const codes = [];
+  for (const num of text.slice(1)) {
+    if (num === "科") {
+      return [true, [1, 2, 3].map((i) => `${category}${i}_all`)];
+    }
+    const group = { 一: "1", 二: "2", 三: "3" }[num];
+    if (group) {
+      codes.push(`${category}${group}_`);
+    } else {
+      throw new Error(text);
+    }
+  }
+  return [false, codes];
+};
+
+/**
  * 対象クラス情報をパースした結果を書き込む
  * @param {Lecture} e
  */
 const parseClass = (e) => {
   console.log(e.code, e.titleJp, e.class);
-  e.class_temp = e.class.split("(").join(" | ");
-  e.class_temp = e.class_temp.split(")").join(" ! ");
-  e.class_temp = e.class_temp.split(",").join(" ");
-  e.class_temp = e.class_temp.split(" ");
-  // console.log(e.class_temp);
-  e.one_grade = [];
-  e.two_grade = [];
-  let grade = 0;
-  const karui = {
-    理一: "s1_",
-    理二: "s2_",
-    理三: "s3_",
-    文一: "l1_",
-    文二: "l2_",
-    文三: "l3_",
-  };
-  for (let i = 0; i < e.class_temp.length; i++) {
-    if (e.class_temp[i] === "1年") {
-      grade = 1;
-    } else if (e.class_temp[i] === "2年") {
-      grade = 2;
+
+  /** @type {string[]} */
+  const temp = e.class
+    .replaceAll("(", " | ")
+    .replaceAll(")", " ! ")
+    .replaceAll(",", " ")
+    .trim()
+    .split(" ");
+  /** @type {number} */
+  let grade = null;
+  /** @type {[string[], string[]]} */
+  const target = [[], []];
+
+  for (let i = 0; i < temp.length; i++) {
+    const grade_ = parseGrade(temp[i]);
+    if (grade_) {
+      grade = grade_;
+      continue;
     }
-    if (grade === 1) {
-      if (e.class_temp[i] === "理科") {
-        e.one_grade.push("s1_all");
-        e.one_grade.push("s2_all");
-        e.one_grade.push("s3_all");
-      } else if (e.class_temp[i] === "文科") {
-        e.one_grade.push("l1_all");
-        e.one_grade.push("l2_all");
-        e.one_grade.push("l3_all");
-      } else if (e.class_temp[i] === "理二三") {
-        if (e.class_temp[i + 1] === "|") {
-          for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-            classes = e.class_temp[j].split("-");
-            if (classes.length === 2) {
-              classes[0] = parseInt(classes[0]);
-              classes[1] = parseInt(classes[1]);
-              for (let k = classes[0]; k <= classes[1]; k++) {
-                e.one_grade.push(karui["理二"] + String(k));
-                e.one_grade.push(karui["理三"] + String(k));
-              }
-            } else if (classes.length === 1) {
-              e.one_grade.push(karui["理二"] + classes[0]);
-              e.one_grade.push(karui["理三"] + classes[0]);
-            }
-          }
-        } else {
-          e.one_grade.push(karui["理二"] + "all");
-          e.one_grade.push(karui["理三"] + "all");
-        }
-      } else if (e.class_temp[i] === "文二三") {
-        if (e.class_temp[i + 1] === "|") {
-          for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-            classes = e.class_temp[j].split("-");
-            if (classes.length === 2) {
-              classes[0] = parseInt(classes[0]);
-              classes[1] = parseInt(classes[1]);
-              for (let k = classes[0]; k <= classes[1]; k++) {
-                e.one_grade.push(karui["文二"] + String(k));
-                e.one_grade.push(karui["文三"] + String(k));
-              }
-            } else if (classes.length === 1) {
-              e.one_grade.push(karui["文二"] + classes[0]);
-              e.one_grade.push(karui["文三"] + classes[0]);
-            }
-          }
-        } else {
-          e.one_grade.push(karui["文二"] + "all");
-          e.one_grade.push(karui["文三"] + "all");
-        }
-      } else if (e.class_temp[i] === "文一二") {
-        if (e.class_temp[i + 1] === "|") {
-          for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-            classes = e.class_temp[j].split("-");
-            if (classes.length === 2) {
-              classes[0] = parseInt(classes[0]);
-              classes[1] = parseInt(classes[1]);
-              for (let k = classes[0]; k <= classes[1]; k++) {
-                e.one_grade.push(karui["文一"] + String(k));
-                e.one_grade.push(karui["文二"] + String(k));
-              }
-            } else if (classes.length === 1) {
-              e.one_grade.push(karui["文一"] + classes[0]);
-              e.one_grade.push(karui["文二"] + classes[0]);
-            }
-          }
-        } else {
-          e.one_grade.push(karui["文一"] + "all");
-          e.one_grade.push(karui["文二"] + "all");
-        }
+    const stream_ = parseStream(temp[i]);
+    if (stream_) {
+      const [isEntire, codes] = stream_;
+      if (isEntire) {
+        target[grade - 1].push(...codes);
       } else {
-        for (let n = 0; n < 6; n++) {
-          if (e.class_temp[i] === Object.keys(karui)[n]) {
-            if (e.class_temp[i + 1] === "|") {
-              for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-                classes = e.class_temp[j].split("-");
-                if (classes.length === 2) {
-                  classes[0] = parseInt(classes[0]);
-                  classes[1] = parseInt(classes[1]);
-                  for (let k = classes[0]; k <= classes[1]; k++) {
-                    e.one_grade.push(karui[e.class_temp[i]] + String(k));
-                  }
-                } else if (classes.length === 1) {
-                  e.one_grade.push(karui[e.class_temp[i]] + classes[0]);
+        if (temp[i + 1] === "|") {
+          i++;
+          for (i++; temp[i] !== "!"; i++) {
+            const classes = temp[i].split("-").map((n) => Number(n));
+            switch (classes.length) {
+              case 1:
+                for (const code of codes) {
+                  target[grade - 1].push(`${code}${classes[0]}`);
                 }
-              }
-            } else {
-              e.one_grade.push(karui[e.class_temp[i]] + "all");
-            }
-          }
-        }
-      }
-    } else if (grade === 2) {
-      if (e.class_temp[i] === "理科") {
-        e.two_grade.push("s1_all");
-        e.two_grade.push("s2_all");
-        e.two_grade.push("s3_all");
-      } else if (e.class_temp[i] === "文科") {
-        e.two_grade.push("l1_all");
-        e.two_grade.push("l2_all");
-        e.two_grade.push("l3_all");
-      } else if (e.class_temp[i] === "理二三") {
-        if (e.class_temp[i + 1] === "|") {
-          for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-            classes = e.class_temp[j].split("-");
-            if (classes.length === 2) {
-              classes[0] = parseInt(classes[0]);
-              classes[1] = parseInt(classes[1]);
-              for (let k = classes[0]; k <= classes[1]; k++) {
-                e.two_grade.push(karui["理二"] + String(k));
-                e.two_grade.push(karui["理三"] + String(k));
-              }
-            } else if (classes.length == 1) {
-              e.two_grade.push(karui["理二"] + classes[0]);
-              e.two_grade.push(karui["理三"] + classes[0]);
+                break;
+              case 2:
+                for (let j = classes[0]; j <= classes[1]; j++) {
+                  for (const code of codes) {
+                    target[grade - 1].push(`${code}${j}`);
+                  }
+                }
+                break;
+              default:
+                throw new Error(`${i}: ${temp}`);
             }
           }
         } else {
-          e.two_grade.push(karui["理二"] + "all");
-          e.two_grade.push(karui["理三"] + "all");
-        }
-      } else if (e.class_temp[i] === "文二三") {
-        if (e.class_temp[i + 1] === "|") {
-          for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-            classes = e.class_temp[j].split("-");
-            if (classes.length === 2) {
-              classes[0] = parseInt(classes[0]);
-              classes[1] = parseInt(classes[1]);
-              for (let k = classes[0]; k <= classes[1]; k++) {
-                e.two_grade.push(karui["文二"] + String(k));
-                e.two_grade.push(karui["文三"] + String(k));
-              }
-            } else if (classes.length === 1) {
-              e.two_grade.push(karui["文二"] + classes[0]);
-              e.two_grade.push(karui["文三"] + classes[0]);
-            }
-          }
-        } else {
-          e.two_grade.push(karui["文二"] + "all");
-          e.two_grade.push(karui["文三"] + "all");
-        }
-      } else {
-        for (let n = 0; n < 6; n++) {
-          if (e.class_temp[i] === Object.keys(karui)[n]) {
-            if (e.class_temp[i + 1] === "|") {
-              for (let j = i + 2; e.class_temp[j] != "!"; j++) {
-                classes = e.class_temp[j].split("-");
-                if (classes.length === 2) {
-                  classes[0] = parseInt(classes[0]);
-                  classes[1] = parseInt(classes[1]);
-                  for (let k = classes[0]; k <= classes[1]; k++) {
-                    e.two_grade.push(karui[e.class_temp[i]] + String(k));
-                  }
-                } else if (classes.length === 1) {
-                  e.two_grade.push(karui[e.class_temp[i]] + classes[0]);
-                }
-              }
-            } else {
-              e.two_grade.push(karui[e.class_temp[i]] + "all");
-            }
+          for (const code of codes) {
+            target[grade - 1].push(`${code}all`);
           }
         }
       }
     }
   }
-  delete e.class_temp;
+  e.one_grade = target[0];
+  e.two_grade = target[1];
 };
 
 /**
